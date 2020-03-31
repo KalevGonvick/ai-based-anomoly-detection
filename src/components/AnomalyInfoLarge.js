cf3fb753a4c84a09dd9c6100945b3d73dcef84ec
AlexMotyka <motykaalex@gmail.com>
2020-03-31 11:25:17 -0400

    resolve merge

---
+++ src/components/AnomalyInfoLarge.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Holder from 'react-holder-component';
+
+/*
+ * This is a large holder picture component
+*/
+
+export class AnomalyInfoLarge extends Component {
+	render() {
+		return(
+			<Holder
+				width="100%"
+				height="600px"
+				/>
+		)
+	}
+}
