6c3a8fc340aed5e7df92bdd8f9777e269615b423
AlexMotyka <motykaalex@gmail.com>
2020-04-01 20:38:03 -0400

    Set port to 80

--- flask-backend/server.py
+++ flask-backend/server.py
@@ -25,5 +25,5 @@ def getData(json):
 
 
 if __name__ == "__main__":
-  socketio.run(app, host='0.0.0.0', port=8000)
+  socketio.run(app, host='0.0.0.0', port=80)
   # app.run(host='0.0.0.0', port=80)
