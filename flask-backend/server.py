d3f5425b9e24398d40efb7a9477ea2872a5ee5c4
AlexMotyka <motykaalex@gmail.com>
2020-04-01 20:37:39 -0400

    Add socketio

--- flask-backend/server.py
+++ flask-backend/server.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, stream_with_context, Response
+from flask_socketio import send, emit, SocketIO
 import sys
 sys.path.append('./temporal-profile/')
 import models
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route("/")
 def hello():
@@ -15,5 +17,13 @@ def graphData():
     model = models.Teachable_AI('./temporal-profile/config.json')
     return Response(stream_with_context(model.run()))
 
+@socketio.on('anomaly-get')
+def getData(json):
+    model = models.Teachable_AI('./temporal-profile/config.json')
+    for chunk in model.run():
+        emit('data chunk', chunk)
+
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=80)
+  socketio.run(app, host='0.0.0.0', port=8000)
+  # app.run(host='0.0.0.0', port=80)
